6343d801e5b082f89695e41943cc011b194066cf
Cameron Beccario <cambecc@nullschool.net>
2013-10-25 21:40:55 +0900

    clarify option descriptions

--- src/main/java/net/nullschool/grib2json/Options.java
+++ src/main/java/net/nullschool/grib2json/Options.java
@@ -15,22 +15,26 @@ import java.io.File;
 @CommandLineInterface(application="grib2json")
 public interface Options {
 
-    @Option(longName="help", shortName={"h", "?"}, description="display help")
+    @Option(longName="help", shortName="h", description="display this help")
     boolean getShowHelp();
 
-    @Option(longName="names", shortName="n", description="print names of the numeric codes")
+    @Option(longName="names", shortName="n", description="print names of numeric codes")
     boolean getPrintNames();
 
-    @Option(longName="data", shortName="d", description="print record data")
+    @Option(longName="data", shortName="d", description="print GRIB record data")
     boolean getPrintData();
 
-    @Option(longName="compact", shortName="c", description="enable compact formatting")
+    @Option(longName="compact", shortName="c", description="enable compact Json formatting")
     boolean isCompactFormat();
 
-    @Option(longName="verbose", shortName="v", description="enable logging")
+    @Option(longName="verbose", shortName="v", description="enable logging to stdout")
     boolean getEnableLogging();
 
-    @Option(longName="output", shortName="o", description="print to specified file", defaultToNull=true)
+    @Option(
+        longName="output",
+        shortName="o",
+        description="write output to the specified file (default is stdout)",
+        defaultToNull=true)
     File getOutput();
 
     @Unparsed(name="FILE", defaultToNull=true)
@@ -53,13 +57,13 @@ public interface Options {
 
     @Option(
         longName={"filter.surface", "fs"},
-        description="select records with this surface type",
+        description="select records with this numeric surface type",
         defaultToNull=true)
     Integer getFilterSurface();
 
     @Option(
         longName={"filter.value", "fv"},
-        description="select records with this surface value",
+        description="select records with this numeric surface value",
         defaultToNull=true)
     Double getFilterValue();
 }
