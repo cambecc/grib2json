13f96c9f53876bc252459f6203a9ff4f581be700
Werner Raath <wraath@csir.co.za>
2016-10-06 11:06:38 +0200

    Improved project structure and improved usage efficiency.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 set -ux
-export PATH=$PATH:/usr/src/app/bin
+export PATH=$PATH:/usr/src/app/compiled/grib2json/bin
 
 TEMP_UID="${TEMP_UID:-1000}"
 useradd -s /bin/false --no-create-home -u ${TEMP_UID} temp
